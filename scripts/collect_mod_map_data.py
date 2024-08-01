76d9f9c8dc9e1032efcf2af4ccea543c6b1d8655
ethano8225 <eoconnor8225@gmail.com>
2024-08-01 15:20:01 -0400

    Update collect_mod_map_data.py
    allows for auto-addition of tile packs

--- scripts/collect_mod_map_data.py
+++ scripts/collect_mod_map_data.py
@@ -1,6 +1,7 @@
 import os
 import yaml
-import get_dep_textures
+import get_mod_dep
+import time
 
 DEFAULT_MAP = '''
     default:
@@ -33,9 +34,9 @@ MAP_TEMPLATE = '''
             {}
         encoding: utf8
         depend_texutres:
-            - '''
+            - default'''
 
-#######
+######
 textpackmatch = {
     2898065560: 'AzaMountainTiles', 
     2804428637: 'BigZombieMonkeys_tile_pack', 
@@ -56,13 +57,27 @@ textpackmatch = {
     2740919036: 'SkizotsTiles',
     2852704777: 'Simon-MDs-Tiles',
     2384329562: 'tkTiles_01',
-    2844685624: "Tryhonesty's Tiles"
+    2844685624: "Tryhonesty's Tiles",
+    1703604612: 'jiggasGreenFire',
+    2463499011: 'Grapeseed',
+    2536865912: 'Blackwood',
+    2991554892: 'TileToItemComverter',
+    1254546530: 'Eerie Country',
+    2595249356: 'Fort Knox linked to Eerie Country',
+    926737806: 'Otr',
+
     }
 
 def addRequiredModpacks(steamid):
-    if steamid in textpackmatch:
-        wholelist="\n\t\t\t- "+ textpackmatch[steamid]
-    return wholelist
+                   #Otr 2nd rte,    Aquatsar,   MM:Server, NW Blockade, Irvingtn Rd, BdfrdFlls,KnoxLtoEerie,Leavenburg -riversdebridge,   coryerdon,
+    falsepositive=["2603239477","2392987599","2725216703","2789257975","2803291537","522891356","2595785944","3085090251","3085088928","2782415851"]
+    if steamid in falsepositive:
+        return ""
+    if int(steamid) in textpackmatch:
+        formatted="\n            - "+textpackmatch.get(int(steamid))
+    else:
+        return "\n            - error: steamid "+str(steamid)
+    return formatted
 ########
 
 def has_texture(tpath):
@@ -103,7 +118,8 @@ def get_mod_conf(mod_root, mod_id):
     return conf
 
 if __name__ == '__main__':
-    with open('../conf.yaml', 'r') as f:
+    total=0
+    with open('conf.yaml', 'r') as f:
         conf = yaml.safe_load(f.read())
     mod_root = conf['mod_root']
     textures = []
@@ -114,31 +130,48 @@ if __name__ == '__main__':
         mods = get_mod_conf(mod_root, mod_id)
         for name in mods:
             if 'map' in mods[name]:
-                maps.append((name, mods[name]))
+                maps.append((name, mods[name],mod_id))
             elif 'texture' in mods[name]:
-                textures.append((name, mods[name]))
+                textures.append((name, mods[name],mod_id))
     
     with open('map_data.yaml', 'w') as f:
+        #################
+        dependsavepath=".\\scripts\\dependsave\\"
+        with open(dependsavepath+"refreshDepends.txt","r") as refresh:
+            if refresh.readline().strip() == "1":
+                saveTime=1
+            else:
+                saveTime=0
+        ##################
         f.write('textures:\n')
-        for name, t in textures:
+        for name, t, mod_id in textures:
             f.write('    {}:'.format(name))
             f.write(TEXTURE_TEMPLATE.format(t['texture']))
             f.write('\n\n')
         f.write('maps:' + DEFAULT_MAP)
-        for name, m in maps:
+        for name, m,mod_id in maps:
             f.write('    {}:'.format(name))
             f.write(MAP_TEMPLATE.format(m['map']))
+            ########
+            startTime = time.time()
+            pathtosave=dependsavepath+str(mod_id)+".txt"
+            if os.path.exists(pathtosave):
+                with open(pathtosave,"r") as depview:
+                    neededSteamIDs = [line.rstrip() for line in depview]
+                    for steamID in neededSteamIDs:
+                        nextitem= addRequiredModpacks(steamID)
+            else:
+                if saveTime==1:
+                    lstOfRecMods=get_mod_dep.get_info(mod_id)
+                    if len(lstOfRecMods) > 0:
+                        with open(pathtosave,"w") as depsave:
+                            for neededSteamID in lstOfRecMods:
+                                depsave.write(str(neededSteamID)+"\n")
+                                nextitem= addRequiredModpacks(neededSteamID)
+                                f.write(nextitem)
+            total+=(time.time()-startTime)
+            ########
             if 'texture' in m:
                 f.write(TEXTURE_TEMPLATE.format(m['texture']))
-            
-            ########
-            for neededSteamID in get_dep_textures.get_info(mod_id):
-                nextitem= addRequiredModpacks(neededSteamID)
-                f.write(nextitem)
-            ########
-            
             f.write('\n\n')
-
-
-
-
+        print("depend_textures took",total,"seconds")
